8241a802ccc3e6278545d3313de248530a605347
Tiberiu Corbu <tiberiu.corbu@gmail.com>
2014-08-26 17:16:43 +0300

    added 3d models for nodes, portal ray node and sourrounding modify functions

--- element_portals_functional/node_api.lua
+++ element_portals_functional/node_api.lua
@@ -12,17 +12,9 @@ function element_portals:swap_node(pos,name)
 	minetest.swap_node(pos,node)
 end
 
--- checks if portal is powered or unpowered and swaps the node acordingly
-function element_portals:swap_if_needed (node, pos, meta, fuel_stack, active_node, inactive_node)
-	if element_portals:is_portal_powered(meta, fuel_stack) and node.name ~= active_node then
-		element_portals:swap_node(pos, active_node)
-	end
-	if not element_portals:is_portal_powered(meta, fuel_stack) and node.name == active_node then
-		element_portals:swap_node(pos, inactive_node)
-	end
-end
 
--- creates a boiler plate for private portals - adds owner in meta and allows only private use   
+
+-- creates a boilerplate for private portals - adds owner in meta and allows only private use   
 function element_portals:build_private_node_template (fuel_stack)
 	return {
 	after_place_node = function(pos, placer)
@@ -106,6 +98,108 @@ function element_portals:build_private_node_template (fuel_stack)
 	}
 end
 
+function element_portals:except_nodes_by_name (node_name, exept_table)
+	if exept_table then
+		for e, except in pairs(exept_table) do
+			if except and string.find(node_name, except) then 
+				return true
+			end
+		end
+	end
+	return false
+end
+
+function element_portals:except_nodes_by_pos_offset(axis_key, axis_offset, params)
+	if params.except_pos then
+		for axis_except_key, axis_offset_excepts in pairs(params.except_pos) do
+			for k, axis_offset_except in pairs(axis_offset_excepts) do
+				if axis_except_key==axis_key and axis_offset_except == axis_offset  then 
+					return true
+				end
+			end
+		end
+	end
+	return false
+end
+
+function element_portals:iterate_offsets (pos, params, callback)
+	local x = pos.x
+	local y = pos.y
+	local z = pos.z
+	local buffer_pos
+	for axis, offset in pairs(params.offsets or {}) do 
+		for index, offset_value in pairs (offset.values or {}) do
+			-- calculate offset position 
+			buffer_pos = {x=x, y=y, z=z}
+			local new_val = buffer_pos[axis]+offset_value
+			buffer_pos[axis]=new_val
+			local affected_node = minetest.get_node(buffer_pos)
+			local affected_node_name = affected_node.name
+			
+			-- extract replacement_node_name from params with fallback 
+			local replacement_node_name
+			if offset.replacement_node_names and offset.replacement_node_names[index] then
+				replacement_node_name = offset.replacement_node_names[index]
+			else 
+				replacement_node_name = "air"
+			end	
+			
+			local node_name_matched = false
+			if offset.match_node_names and offset.match_node_names[index] then
+			
+				local match_node_name = offset.match_node_names[index]
+				node_name_matched = affected_node_name ==  match_node_name
+
+			end	
+			-- compute exceptions 
+			local except = element_portals:except_nodes_by_name(affected_node_name, params.except_nodes_containing_in_name)
+			if (not except or node_name_matched) and affected_node_name ~= replacement_node_name then 
+				callback(buffer_pos, node_name_matched, replacement_node_name, affected_node_name)
+			end	
+		end
+	end
+end
+
+
+function element_portals:handle_dig_sourroundings_on_activate(node_name, pos, params)
+	if node_name == params.active_node and params.dig_sourroundings_on_activate then
+		local callback = function( buffer_pos, node_name_matched, replacement_node_name, affected_node_name)
+			minetest.dig_node(buffer_pos)
+			if replacement_node_name and replacement_node_name~="air" then 
+				node = minetest.get_node(buffer_pos)
+				if node.name ~= replacement_node_name then
+					minetest.set_node(buffer_pos, {name=replacement_node_name})
+				end
+			end
+		end
+		element_portals:iterate_offsets(pos, params.dig_sourroundings_on_activate, callback)
+	end
+end
+
+function element_portals:handle_clear_sourroundings_on_deactivate(node_name, pos, params)
+	if node_name == params.inactive_node and params.clear_sourroundings_on_deactivate then
+		local callback = function( buffer_pos, node_name_matched, replacement_node_name, affected_node_name)
+			
+			if node_name_matched then 
+				
+				minetest.set_node(buffer_pos, {name="air"})
+			end
+		end
+		element_portals:iterate_offsets(pos, params.clear_sourroundings_on_deactivate, callback)
+	end
+end
+
+
+-- checks if portal is powered or unpowered and swaps the node acordingly
+function element_portals:swap_if_needed (node, pos, meta, fuel_stack, active_node, inactive_node)
+	if element_portals:is_portal_powered(meta, fuel_stack) and node.name ~= active_node then
+		element_portals:swap_node(pos, active_node)
+	end
+	if not element_portals:is_portal_powered(meta, fuel_stack) and node.name == active_node then
+		element_portals:swap_node(pos, inactive_node)
+	end
+end
+
 -- Configures the template for the node and expands it with the overwrites
 -- parameter. 
 -- params should contain the `active_node` ex: `default:tree` the `inactive_node` and the `fuel` stack string ex `default:wood 1` 
@@ -118,15 +212,18 @@ function element_portals:create_private_portal_node(name, overwrites, params)
 	for k,v in pairs(overwrites) do
 		node[k]=v
 	end
+	-- register node here
 	minetest.register_node(name, node)
-	
+	-- register interval for active inactive node swaping
 	minetest.register_abm({
 		nodenames = {params.inactive_node, params.active_node},
 		interval = 1.0,
 		chance = 1,
 		action = function(pos, node, active_object_count, active_object_count_wider)
-			local meta = minetest.get_meta(pos)
+			 local meta = minetest.get_meta(pos)
 			 element_portals:swap_if_needed(node, pos, meta, params.fuel, params.active_node, params.inactive_node)
+			 element_portals:handle_dig_sourroundings_on_activate(node.name, pos, params)
+			 element_portals:handle_clear_sourroundings_on_deactivate(node.name, pos, params);
 		end
 	})
 	
--- liquid_portals/nodes.lua
+++ liquid_portals/nodes.lua
@@ -1,7 +1,136 @@
+-- GENERATED CODE
+-- Node Box Editor, version 0.7 - Iron
+ 
+local liquid_portal_nodebox = {
+		type = "fixed",
+		fixed = {
+			{-0.125, -0.375, -0.125, 0.125, 0.375, 0.125}, -- NodeBox6
+			{-0.25, -0.3125, -0.125, 0.25, 0.3125, 0.125}, -- NodeBox7
+			{-0.125, -0.3125, -0.25, 0.125, 0.3125, 0.25}, -- NodeBox9
+			{-0.1875, -0.3125, -0.1875, 0.1875, 0.3125, 0.1875}, -- NodeBox15
+			{-0.3125, -0.25, -0.125, 0.3125, 0.25, 0.125}, -- NodeBox16
+			{-0.125, -0.25, -0.3125, 0.125, 0.25, 0.3125}, -- NodeBox17
+			{-0.1875, -0.25, -0.25, 0.1875, 0.25, 0.25}, -- NodeBox19
+			{-0.25, -0.25, -0.1875, 0.25, 0.25, 0.1875}, -- NodeBox20
+			{-0.25, -0.1875, -0.25, 0.25, 0.1875, 0.25}, -- NodeBox21
+			{-0.3125, -0.1875, -0.1875, 0.3125, 0.1875, 0.1875}, -- NodeBox22
+			{-0.1875, -0.1875, -0.3125, 0.1875, 0.1875, 0.3125}, -- NodeBox23
+			{-0.125, -0.125, -0.375, 0.125, 0.125, 0.375}, -- NodeBox24
+			{-0.375, -0.125, -0.125, 0.375, 0.125, 0.125}, -- NodeBox25
+			{-0.3125, -0.125, -0.1875, 0.3125, 0.125, 0.1875}, -- NodeBox26
+			{-0.1875, -0.125, -0.3125, 0.1875, 0.125, 0.3125}, -- NodeBox27
+			{-0.25, -0.125, -0.25, 0.25, 0.125, 0.25}, -- NodeBox28
+			{-0.5, -0.5, -0.5, -0.3125, 0.5, -0.4375}, -- NodeBox30
+			{-0.5, -0.5, -0.4375, -0.4375, 0.5, -0.3125}, -- NodeBox31
+			{-0.5, -0.5, 0.4375, -0.3125, 0.5, 0.5}, -- NodeBox32
+			{-0.5, -0.5, 0.3125, -0.4375, 0.5, 0.4375}, -- NodeBox33
+			{0.4375, -0.5, 0.3125, 0.5, 0.5, 0.5}, -- NodeBox34
+			{0.3125, -0.5, 0.4375, 0.5, 0.5, 0.5}, -- NodeBox35
+			{0.3125, -0.5, -0.5, 0.5, 0.5, -0.4375}, -- NodeBox36
+			{0.4375, -0.5, -0.4375, 0.5, 0.5, -0.3125}, -- NodeBox37
+			{-0.5, -0.5, -0.5, 0.5, -0.4375, -0.3125}, -- NodeBox38
+			{-0.5, -0.5, 0.3125, 0.5, -0.4375, 0.5}, -- NodeBox39
+			{0.3125, -0.5, -0.5, 0.5, -0.4375, 0.5}, -- NodeBox40
+			{-0.5, -0.5, -0.5, -0.3125, -0.4375, 0.5}, -- NodeBox41
+			{-0.5, -0.5, -0.5, 0.5, -0.3125, -0.4375}, -- NodeBox42
+			{0.4375, -0.5, -0.5, 0.5, -0.3125, 0.5}, -- NodeBox43
+			{-0.5, -0.5, -0.5, -0.4375, -0.3125, 0.5}, -- NodeBox44
+			{-0.5, -0.5, 0.4375, 0.5, -0.3125, 0.5}, -- NodeBox45
+			{-0.5, 0.4375, -0.5, -0.3125, 0.5, 0.5}, -- NodeBox46
+			{-0.5, 0.4375, 0.3125, 0.5, 0.5, 0.5}, -- NodeBox47
+			{0.3125, 0.4375, -0.5, 0.5, 0.5, 0.5}, -- NodeBox48
+			{-0.5, 0.4375, -0.5, 0.5, 0.5, -0.3125}, -- NodeBox49
+			{-0.5, 0.3125, 0.4375, 0.5, 0.5, 0.5}, -- NodeBox50
+			{-0.5, 0.3125, -0.5, -0.4375, 0.5, 0.5}, -- NodeBox51
+			{-0.5, 0.3125, -0.5, 0.5, 0.5, -0.4375}, -- NodeBox52
+			{0.4375, 0.3125, -0.5, 0.5, 0.5, 0.5}, -- NodeBox53
+		}
+	} 
+
+local ray_node_fixed_box = {
+			{0.4375, -0.5, -0.125, 0.5, 0.5, 0.125}, -- NodeBox1
+			{0.375, -0.5, 0.125, 0.4375, 0.5, 0.25}, -- NodeBox2
+			{0.3125, -0.5, 0.25, 0.375, 0.5, 0.3125}, -- NodeBox3
+			{0.25, -0.5, 0.25, 0.3125, 0.5, 0.375}, -- NodeBox4
+			{0.125, -0.5, 0.375, 0.25, 0.5, 0.4375}, -- NodeBox5
+			{-0.125, -0.5, 0.4375, 0.125, 0.5, 0.5}, -- NodeBox6
+			{-0.25, -0.5, 0.375, -0.125, 0.5, 0.4375}, -- NodeBox7
+			{-0.3125, -0.5, 0.25, -0.25, 0.5, 0.375}, -- NodeBox8
+			{-0.375, -0.5, 0.25, -0.3125, 0.5, 0.3125}, -- NodeBox9
+			{-0.4375, -0.5, 0.125, -0.375, 0.5, 0.25}, -- NodeBox10
+			{-0.5, -0.5, -0.125, -0.4375, 0.5, 0.125}, -- NodeBox11
+			{-0.125, -0.5, -0.5, 0.125, 0.5, -0.4375}, -- NodeBox12
+			{-0.25, -0.5, -0.4375, -0.125, 0.5, -0.375}, -- NodeBox13
+			{-0.4375, -0.5, -0.25, -0.375, 0.5, -0.125}, -- NodeBox14
+			{-0.375, -0.5, -0.3125, -0.25, 0.5, -0.25}, -- NodeBox15
+			{-0.3125, -0.5, -0.375, -0.25, 0.5, -0.3125}, -- NodeBox18
+			{0.125, -0.5, -0.4375, 0.25, 0.5, -0.375}, -- NodeBox19
+			{0.375, -0.5, -0.25, 0.4375, 0.5, -0.125}, -- NodeBox20
+			{0.25, -0.5, -0.375, 0.375, 0.5, -0.3125}, -- NodeBox21
+			{0.3125, -0.5, -0.3125, 0.375, 0.5, -0.25}, -- NodeBox22
+		}
+
+
+-- GENERATED CODE
+-- Node Box Editor, version 0.7 - Iron
+-- Namespace: test
+
+local register_portal_ray = function(name ,texture, post_effect_color)
+
+	minetest.register_node(name, {
+		tiles = {
+			"ray_y_tile.png",
+			"ray_y_tile.png",
+			texture,
+			texture,
+			texture,
+			texture
+		},
+		paramtype="light",
+		use_texture_alpha = true,
+		walkable = false,
+		pointable = false,
+		diggable = false,
+		drawtype = "nodebox",
+		is_ground_content = true,
+		light_source = LIGHT_MAX - 1,
+		post_effect_color = post_effect_color, 
+		node_box = {
+			type = "fixed",
+			fixed = ray_node_fixed_box
+		}
+	})
+end
+register_portal_ray("liquid_portals:water_portal_ray_bottom", "water_portal_ray_bottom.png", { r=125, g=198, b=198, a=50 })
+register_portal_ray("liquid_portals:water_portal_ray_top", "water_portal_ray_top.png", { r=125, g=198, b=198, a=50 })
 local water_portal_params =  {
  fuel = "bucket:bucket_water 1",
  active_node = "liquid_portals:water_portal_active", 
- inactive_node = "liquid_portals:water_portal"
+ inactive_node = "liquid_portals:water_portal",
+ 
+ ray_bottom = {
+ 	name = "liquid_portals:water_portal_ray_bottom",
+ 	texture = "water_portal_ray_bottom.png"
+ },
+ ray_top = {
+ 	name = "liquid_portals:water_portal_ray_top",
+ 	texture = "water_portal_ray_top.png"
+ },
+ dig_sourroundings_on_activate = {
+ 	except_nodes_containing_in_name = {"water_source", "water_flowing", "portal_ray" },
+ 	offsets = {
+ 		-- if unspecified replacement_node_names, air is the default 
+ 		x = {values = {-1, 1}},
+ 		z = {values = {-1, 1}},
+ 		y = {values = {-1, 1, 2}, replacement_node_names = {"air", "liquid_portals:water_portal_ray_bottom", "liquid_portals:water_portal_ray_top"}}
+ 	}
+ },
+ clear_sourroundings_on_deactivate = {
+ 	except_nodes_containing_in_name = {"water_source", "water_flowing", "portal_ray" },
+ 	offsets = {
+ 		y = {values = {1, 2}, match_node_names = {"liquid_portals:water_portal_ray_bottom", "liquid_portals:water_portal_ray_top"}}
+ 	}
+ }
 }
 
 element_portals:create_private_portal_node("liquid_portals:water_portal", {
@@ -9,7 +138,11 @@ element_portals:create_private_portal_node("liquid_portals:water_portal", {
 	tiles = {
 		{name="portal_water_anim.png", animation={type="vertical_frames", aspect_w=16, aspect_h=16, length=2.0}}
 	},
-	is_ground_content = false,
+	drawtype = "nodebox",
+	use_texture_alpha = false,
+	is_ground_content = true,
+	paramtype="light",
+	node_box = liquid_portal_nodebox,
 	groups = {cracky=3}
 }, water_portal_params)
 
@@ -56,21 +189,21 @@ element_portals:create_private_portal_node("liquid_portals:water_portal_active",
 	tiles = {
 		{name="portal_water_anim_active.png", animation={type="vertical_frames", aspect_w=16, aspect_h=16, length=2.0}}
 	},
-	
-	alpha = WATER_ALPHA,
-	paramtype = "light",
+	drawtype = "nodebox",
+	use_texture_alpha = false,
+	is_ground_content = true,
+	paramtype="light",
+	node_box = liquid_portal_nodebox,
 	liquidtype = "source",
 	liquid_alternative_flowing = "liquid_portals:water_flowing",
 	liquid_alternative_source = "liquid_portals:water_portal_active",
-
-	post_effect_color = {a=64, r=100, g=100, b=200},
 	groups = {cracky=3, puts_out_fire=1},
 }, water_portal_params)
 
 
+
 minetest.register_node("liquid_portals:lava_flowing", {
-	description = "Flowing Lava",
-	
+	description = "Flowing Lava",	
 	drawtype = "flowingliquid",
 	tiles = {"default_lava.png"},
 	special_tiles = {
@@ -104,16 +237,40 @@ minetest.register_node("liquid_portals:lava_flowing", {
 	groups = {lava=3, liquid=2, hot=3, igniter=1, not_in_creative_inventory=1},
 })
 
-
+register_portal_ray("liquid_portals:lava_portal_ray_bottom", "lava_portal_ray_bottom.png",  { r=255, g=172, b=93, a=50 })
+register_portal_ray("liquid_portals:lava_portal_ray_top", "lava_portal_ray_top.png",   { r=255, g=172, b=93, a=50 })
 local lava_portal_params =  {
  fuel = "bucket:bucket_lava 1",
  active_node = "liquid_portals:lava_portal_active", 
- inactive_node = "liquid_portals:lava_portal"
+ inactive_node = "liquid_portals:lava_portal",
+ leaks = "liquid_portals:lava_flowing",
+ 
+ dig_sourroundings_on_activate = {
+ 	except_nodes_containing_in_name = {"lava_source", "lava_flowing", "portal_ray" },
+ 	offsets = {
+ 		-- if unspecified replacement_node_names, air is the default 
+ 		x = {values = {-1, 1}},
+ 		z = {values = {-1, 1}},
+ 		y = {values = {-1, 1, 2}, replacement_node_names = {"air", "liquid_portals:lava_portal_ray_bottom", "liquid_portals:lava_portal_ray_top"}}
+ 	}
+ },
+ clear_sourroundings_on_deactivate = {
+ 	except_nodes_containing_in_name = {"lava_source", "lava_flowing", "portal_ray" },
+ 	offsets = {
+ 		y = {values = {1, 2}, match_node_names = {"liquid_portals:lava_portal_ray_bottom", "liquid_portals:lava_portal_ray_top"}}
+ 	}
+ }
+ 
+ 
 }
 
 element_portals:create_private_portal_node("liquid_portals:lava_portal", {
 	description = "Lava Portal",
-
+	drawtype = "nodebox",
+	use_texture_alpha = false,
+	is_ground_content = true,
+	paramtype="light",
+	node_box = liquid_portal_nodebox,
 	tiles = {
 		{name="portal_lava_anim.png", animation={type="vertical_frames", aspect_w=16, aspect_h=16, length=2.0}}
 	},
@@ -127,9 +284,14 @@ element_portals:create_private_portal_node("liquid_portals:lava_portal_active", 
 	tiles = {
 		{name="portal_lava_anim_active.png", animation={type="vertical_frames", aspect_w=16, aspect_h=16, length=2.0}}
 	},
-	is_ground_content = false,
+	drawtype = "nodebox",
+	use_texture_alpha = false,
+	is_ground_content = true,
+	paramtype="light",	
+	node_box = liquid_portal_nodebox,	
+	
+	
 	groups = {cracky=3,hot=3, igniter=1},
-	paramtype = "light",
 	light_source = LIGHT_MAX - 1,
 	liquidtype = "source",
 	liquid_alternative_flowing = "liquid_portals:lava_flowing",
